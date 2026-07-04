3ab348393d375650495b095bcb52dbef431c01d9
Hojat Gazestani <39824268+hojat-gazestani@users.noreply.github.com>
2026-07-04 14:08:34 +0330

    Use the RRF algorithm to rank retrived docs

---
+++ rag-systems/fusion-reranker/main.py
@@ -0,0 +1,41 @@
+from colorama import Fore
+from query import query 
+
+# https://medium.com/artificial-corner/re-ranking-is-all-you-need-7a6b1e586d48
+def start():
+    instructions = (
+        """How can I help you today ?\n"""
+    )
+    print(Fore.BLUE + "\n\x1B[3m" + instructions + "\x1B[0m" + Fore.RESET)
+
+    print("MENU")
+    print("====")
+    print("[1]- Ask a question")
+    print("[2]- Exit")
+    choice = input("Enter your choice: ")
+    if choice == "1":
+        ask()
+    elif choice == "2":
+        print("Goodbye!")
+        exit()
+    else:
+        print("Invalid choice")
+        start()
+
+
+def ask():
+    while True:
+        user_input = input("Q: ")
+        # Exit
+        if user_input == "x":
+            start()
+        else:
+
+            response = query(user_input)
+            print(Fore.BLUE + "A: " + response + Fore.RESET)
+            print(Fore.WHITE + 
+                  "\n-------------------------------------------------")
+
+
+if __name__ == "__main__":
+    start()
